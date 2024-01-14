54a077eaf7e5f3d8ea35ec8618f28b1b47264a5e
Alexander Muli <mulingealexmuli@gmail.com>
2024-01-14 23:09:16 +0300

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,11 +14,9 @@ import { AuthGuard } from './guards/auth.guard';
                     { path: 'products', loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule) },
                     { path: 'profile', loadChildren: () => import('./components/profile/profile.module').then(m => m.ProfileModule) },
                     { path: 'uikit', loadChildren: () => import('./components/uikit/uikit.module').then(m => m.UIkitModule), canActivate: [AuthGuard] },
-                    { path: 'pages', loadChildren: () => import('./components/pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] }
                 ]
             },
             { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -170,53 +170,6 @@ export class AppMenuComponent implements OnInit {
         ],
       },
       {
-        label: "Pages",
-        icon: "pi pi-fw pi-briefcase",
-        items: [
-          {
-            label: "Landing",
-            icon: "pi pi-fw pi-globe",
-            routerLink: ["/landing"],
-          },
-          {
-            label: "Auth",
-            icon: "pi pi-fw pi-user",
-            items: [
-              {
-                label: "Login",
-                icon: "pi pi-fw pi-sign-in",
-                routerLink: ["/auth/login"],
-              },
-              {
-                label: "Error",
-                icon: "pi pi-fw pi-times-circle",
-                routerLink: ["/auth/error"],
-              },
-              {
-                label: "Access Denied",
-                icon: "pi pi-fw pi-lock",
-                routerLink: ["/auth/access"],
-              },
-            ],
-          },
-          {
-            label: "Crud",
-            icon: "pi pi-fw pi-pencil",
-            routerLink: ["/pages/crud"],
-          },
-          {
-            label: "Not Found",
-            icon: "pi pi-fw pi-exclamation-circle",
-            routerLink: ["/notfound"],
-          },
-          {
-            label: "Empty",
-            icon: "pi pi-fw pi-circle-off",
-            routerLink: ["/pages/empty"],
-          },
-        ],
-      },
-      {
         label: "Hierarchy",
         items: [
           {
