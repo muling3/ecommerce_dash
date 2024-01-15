44c64d5a8d00896f7f0fbd8dd895d9b7ce3bc68c
Alexander Muli <mulingealexmuli@gmail.com>
2024-01-15 14:34:09 +0300

    added product list markup and logic

--- src/app/components/products/products-list/list.module.ts
+++ src/app/components/products/products-list/list.module.ts
@@ -3,9 +3,23 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { ProductsListComponent } from "./products-list.component";
 import { ListRoutingModule } from "./list-routing.module";
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { RatingModule } from "primeng/rating";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ListRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    RatingModule,
+    ToastModule,
+    ListRoutingModule,
+  ],
   declarations: [ProductsListComponent],
 })
 export class ProductListModule {}
--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { MessageService } from "primeng/api";
+import { Table } from "primeng/table";
+import { Product } from "src/app/api/product";
+import { ProductService } from "src/app/service/product.service";
 
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  selector: "app-products-list",
+  templateUrl: "./products-list.component.html",
+  styleUrls: ["./products-list.component.scss"],
 })
 export class ProductsListComponent {
+  products: Product[] = [];
 
+  loading: boolean = true;
+
+  @ViewChild("filter") filter!: ElementRef;
+
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (res: any) => {
+        this.products = res.data;
+        this.loading = false;
+      },
+      error: (err: any) => {
+        console.log("err", err);
+        this.loading = false;
+        this.messageService.add({
+          severity: "error",
+          summary: "Error Fetching Products!",
+          detail: err.error.message ? err.error.message : err.message,
+        });
+      },
+    });
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, "contains");
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = "";
+  }
 }
